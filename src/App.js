23359c908f5036aa0fa7ea7306ede10bf8222bdc
caiocsper <ccpereira0498@gmail.com>
2022-08-05 16:14:13 -0300

    Fix error de Lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 class App extends Component {
   render() {
