67c21710f76d29f1c408ad3eb2b0f1eb39589d6f
caiocsper <ccpereira0498@gmail.com>
2022-08-05 16:58:16 -0300

    Fix erros de Lint

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
-import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-
 
 class App extends Component {
   render() {
