bfe845c6f152403926e3b6e2a0bbaf8cb757172c
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-08-05 18:39:54 -0300

    css da lista de categorias

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import CategoryItem from './CategoryItem';
+import './CategoryList.css';
 
 export default class CategoryList extends Component {
   render() {
     const { categoriesList } = this.props;
     return (
-      <aside>
+      <aside className="categoriesList">
         <h3>Categorias: </h3>
         {categoriesList.map((category) => (
           <CategoryItem key={ category.id } name={ category.name } />
