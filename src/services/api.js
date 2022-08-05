653a7eaedb46e09c05b6bc9831504f1f889bff94
Leckar <gusleckar@gmail.com>
2022-08-05 15:18:48 -0300

    initial commit, requisite 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
+const baseURL = 'https://api.mercadolibre.com/sites/MLB/';
+
 export async function getCategories() {
-  // Implemente aqui
+  const data = await fetch(`${baseURL}categories`);
+  const obj = await data.json();
+  return obj;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const data = await fetch(`${baseURL}search?category=${categoryId}&q=${query}`);
+  const obj = await data.json();
+  return obj;
 }
