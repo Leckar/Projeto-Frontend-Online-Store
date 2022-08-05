77eab1b6ad7a311f9cf09f105cfc22252e6ef51d
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-08-05 16:08:50 -0300

    inicio

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class CategoryList extends Component {
+  render() {
+    const { categoriesList } = this.props;
+    return (
+      <h2>Categorias:</h2>
+    );
+  }
+}
